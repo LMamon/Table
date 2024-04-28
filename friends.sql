d0a521fbf475e7d0d45a50bf8d04f04bd9f03e41
Louis M <knit-close.0o@icloud.com>
2024-04-28 00:07:27 -0800

    table and db connection

--- friends.sql
+++ friends.sql
@@ -1,3 +1,8 @@
-CREATE TABLE friends(id INTEGER, name TEXT, birthday DATE);
 INSERT INTO friends(id, name, birthday) VALUES (1, 'Ororo Munroe', '1940-05-30');
+SELECT * FROM friends;
+INSERT INTO friends (id, name, birthday) VALUES(2,'Pied Piper', '1760-11-02');
+INSERT INTO friends (id, name, birthday) VALUES(3, 'Sound Garden', '2000-03-15');
+UPDATE friends SET name = 'Storm' WHERE id = 1;
+ALTER TABLE friends ADD COLUMN email TEXT;
+DELETE FROM friends where name = 'Storm';
 SELECT * FROM friends;
